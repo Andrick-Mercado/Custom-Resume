6e574beac53e0bb190701425c2aedfa5660304ac
Andrick <andrickmercado17@gmail.com>
2023-10-20 19:25:28 -0700

    Updated default pages

--- Shared/MainLayout.razor.cs
+++ Shared/MainLayout.razor.cs
@@ -2,87 +2,88 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
-namespace CustomResumeBlazor.Shared
+namespace CustomResumeBlazor.Shared;
+
+public partial class MainLayout
 {
-    public partial class MainLayout
+    [Inject]
+    public ILocalStorageService LocalStorage { get; set; }
+
+    protected async override Task OnInitializedAsync()
     {
-        [Inject]
-        public ILocalStorageService LocalStorage { get; set; }
-
-        private MudTheme _currentTheme;
-        private MudTheme _darkTheme = new()
+        if (await LocalStorage.ContainKeyAsync("theme"))
         {
-            Palette = new() 
-            {
-                AppbarBackground = "#0097FF",
-                AppbarText = "#FFFFFF",
-                Primary = "#007CD1",
-                TextPrimary = "#FFFFFF",
-                Background = "#001524",
-                TextSecondary = "#E2EEF6",
-                DrawerBackground = "#093958",
-                DrawerText = "#FFFFFF",
-                Surface = "#093958",
-                ActionDefault = "#0C1217",
-                ActionDisabled = "#2F678C",
-                TextDisabled = "#B0B0B0"
-            }
-        };
-        private MudTheme _lightTheme = new MudTheme
+            _isDarkCurrentTheme = await LocalStorage.GetItemAsync<bool>("theme");
+        }
+        else
         {
-            Palette = new Palette
-            {
-                AppbarBackground = "#0097FF",
-                AppbarText = "#FFFFFF",
-                Primary = "#007CD1",
-                TextPrimary = "#0C1217",
-                Background = "#F4FDFF",
-                TextSecondary = "#0C1217",
-                DrawerBackground = "#C5E5FF",
-                DrawerText = "#0C1217",
-                Surface = "#E4FAFF",
-                ActionDefault = "#0C1217",
-                ActionDisabled = "#2F678C",
-                TextDisabled = "#676767",
-            }
-        };
-        private bool _isDarkCurrentTheme = true;
-        private bool _drawerOpen = true;
-
-        protected async override Task OnInitializedAsync()
-        {
-            if (await LocalStorage.ContainKeyAsync("theme"))
-            {
-                _isDarkCurrentTheme = await LocalStorage.GetItemAsync<bool> ("theme");
-            }
-            else
-            {
-                _isDarkCurrentTheme = true;
-            }
-
-            _currentTheme =_isDarkCurrentTheme ? _darkTheme : _lightTheme;
+            _isDarkCurrentTheme = true;
         }
 
-        private void DrawerToggle()
-        {
-            _drawerOpen = !_drawerOpen;
-        }
-
-        private async Task ChangeThemeAsync()
-        {
-            if (!_isDarkCurrentTheme)
-            {
-                _currentTheme = _darkTheme;
-            }
-            else
-            {
-                _currentTheme = _lightTheme;
-            }
-
-
-            _isDarkCurrentTheme = !_isDarkCurrentTheme;
-            await LocalStorage.SetItemAsync("theme", _isDarkCurrentTheme);
-        }
-
+        _currentTheme = _isDarkCurrentTheme ? _darkTheme : _lightTheme;
     }
+
+    private void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+
+    private async Task ChangeThemeAsync()
+    {
+        if (!_isDarkCurrentTheme)
+        {
+            _currentTheme = _darkTheme;
+        }
+        else
+        {
+            _currentTheme = _lightTheme;
+        }
+
+
+        _isDarkCurrentTheme = !_isDarkCurrentTheme;
+        await LocalStorage.SetItemAsync("theme", _isDarkCurrentTheme);
+    }
+
+    #region Private fields
+    private MudTheme _currentTheme;
+    private MudTheme _darkTheme = new()
+    {
+        Palette = new()
+        {
+            AppbarBackground = "#0097FF",
+            AppbarText = "#FFFFFF",
+            Primary = "#007CD1",
+            TextPrimary = "#FFFFFF",
+            Background = "#001524",
+            TextSecondary = "#E2EEF6",
+            DrawerBackground = "#093958",
+            DrawerText = "#FFFFFF",
+            Surface = "#093958",
+            ActionDefault = "#0C1217",
+            ActionDisabled = "#2F678C",
+            TextDisabled = "#B0B0B0"
+        }
+    };
+    private MudTheme _lightTheme = new MudTheme
+    {
+        Palette = new Palette
+        {
+            AppbarBackground = "#0097FF",
+            AppbarText = "#FFFFFF",
+            Primary = "#007CD1",
+            TextPrimary = "#0C1217",
+            Background = "#F4FDFF",
+            TextSecondary = "#0C1217",
+            DrawerBackground = "#C5E5FF",
+            DrawerText = "#0C1217",
+            Surface = "#E4FAFF",
+            ActionDefault = "#0C1217",
+            ActionDisabled = "#2F678C",
+            TextDisabled = "#676767",
+        }
+    };
+    private bool _isDarkCurrentTheme = true;
+    private bool _drawerOpen = true;
+    #endregion
 }
+
