7ea39618aa504d17844a0f850df20491a9042a6a
Andrick Mercado <97013965+Andrick-Mercado@users.noreply.github.com>
2024-01-06 20:41:49 -0800

    Added all card generic page

---
+++ Client/Domain/RouteMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomResumeBlazor.Domain;
+
+public static class RouteMapper
+{
+    public static string GetAllCardsRoute(string endpoint) => $"defaultviewer/{endpoint}";
+}
+
---
+++ Client/Pages/AllCardsPage.razor.cs
@@ -0,0 +1,33 @@
+﻿using CustomResumeBlazor.Domain;
+using CustomResumeBlazor.Infrastructure;
+using Microsoft.AspNetCore.Components;
+
+namespace CustomResumeBlazor.Pages;
+
+public partial class AllCardsPage
+{
+    [Parameter]
+    public string ClientRouteName { get; set; }
+
+    [Inject]
+    private IDatabaseService DatabaseService { get; set; }
+
+    private bool hasLoaded = false;
+    private WebsiteDatabaseData websiteDatabaseData;
+    private OtherPages currentPage;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (hasLoaded) return;
+
+        await Task.Delay(3000);
+        websiteDatabaseData = await DatabaseService.GetWebsiteDatabaseDataAsync();
+        currentPage = websiteDatabaseData.WebsiteData.OtherPages.FirstOrDefault(x => x.Endpoint == ClientRouteName);
+
+        if (currentPage is not null)
+            hasLoaded = true;
+
+
+        StateHasChanged();
+    }
+}
