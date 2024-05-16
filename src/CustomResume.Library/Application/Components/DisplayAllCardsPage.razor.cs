6566340c57a91ae34ad4a8cadefd791c54ef2e87
Andrick Mercado <97013965+Andrick-Mercado@users.noreply.github.com>
2024-05-15 22:33:57 -0700

    Patch github actions
    Fixed
    * Issue were getting the name of branch rather than the repo name

---
+++ src/CustomResume.Library/Application/Components/DisplayAllCardsPage.razor.cs
@@ -0,0 +1,60 @@
+using CustomResumeBlazor.Domain;
+using CustomResumeBlazor.Infrastructure;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CustomResume.Library.Application.Components;
+
+public partial class DisplayAllCardsPage 
+{
+    [Parameter]
+    public string ClientRouteName { get; set; } = default!;
+
+    [Inject]
+    private IWebsiteRepo WebsiteRepo { get; set; } = default!;
+
+    private bool _hasLoaded = false;
+    private WebsiteData? _websiteDatabaseData;
+    private OtherPages? _currentPage;
+
+    protected override void OnInitialized()
+    {
+        if (_hasLoaded && ClientRouteName == _currentPage?.Endpoint) return;
+
+        _websiteDatabaseData = WebsiteRepo.GetWebsiteData();
+        _currentPage = _websiteDatabaseData.OtherPages.FirstOrDefault(x => x.Endpoint == ClientRouteName);
+
+        _hasLoaded = _currentPage is not null;
+        StateHasChanged();
+    }
+
+    protected override void OnParametersSet()
+    {
+        if (_websiteDatabaseData is null)
+        {
+            _websiteDatabaseData = WebsiteRepo.GetWebsiteData();
+            _currentPage = _websiteDatabaseData.OtherPages.FirstOrDefault(x => x.Endpoint == ClientRouteName);
+        }
+        else if (_currentPage is not null && ClientRouteName != _currentPage.Endpoint)
+        {
+            _currentPage = _websiteDatabaseData.OtherPages.FirstOrDefault(x => x.Endpoint == ClientRouteName);
+        }
+        else
+        {
+            _currentPage ??= _websiteDatabaseData.OtherPages.FirstOrDefault(x => x.Endpoint == ClientRouteName);
+        }
+
+        _hasLoaded = _currentPage is not null;
+        StateHasChanged();
+    }
+
+    private IEnumerable<IGrouping<string, Card>> GetCardsGroupedByCardName()
+    {
+        return _currentPage?.Cards.GroupBy(x => x.Name) ?? Array.Empty<IGrouping<string, Card>>();
+    }
+
+    private Card GetFirstCard()
+    {
+        return _currentPage?.Cards.FirstOrDefault() ?? new Card();
+    }
+}
---
+++ src/CustomResume.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomResume.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
