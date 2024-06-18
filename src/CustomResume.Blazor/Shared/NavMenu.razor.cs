3db80a8255299c29649ab6724a109e10d69c8261
Andrick Mercado <97013965+Andrick-Mercado@users.noreply.github.com>
2024-06-17 20:13:14 -0700

    Test 19

--- src/CustomResume.Blazor/Shared/NavMenu.razor.cs
+++ src/CustomResume.Blazor/Shared/NavMenu.razor.cs
@@ -2,25 +2,24 @@ using CustomResumeBlazor.Domain;
 using CustomResumeBlazor.Infrastructure;
 using Microsoft.AspNetCore.Components;
 
-namespace CustomResume.Blazor.Shared
+namespace CustomResume.Blazor.Shared;
+
+public partial class NavMenu
 {
-    public partial class NavMenu
+    [Inject]
+    private IWebsiteRepo WebsiteRepo { get; set; } = default!;
+
+    private bool hasLoaded = false;
+    private WebsiteData? websiteData;
+    private IOrderedEnumerable<OtherPages>? sortedOtherPages;
+
+    protected override void OnInitialized()
     {
-        [Inject]
-        private IWebsiteRepo WebsiteRepo { get; set; } = default!;
+        if (hasLoaded) return;
 
-        private bool hasLoaded = false;
-        private WebsiteData? websiteData;
-        private IOrderedEnumerable<OtherPages>? sortedOtherPages;
-
-        protected override void OnInitialized()
-        {
-            if (hasLoaded) return;
-
-            websiteData = WebsiteRepo.GetWebsiteData();
-            hasLoaded = websiteData is not null;
-            sortedOtherPages = websiteData?.OtherPages?.OrderBy(x => x.SortOrder);
-            StateHasChanged();
-        }
+        websiteData = WebsiteRepo.GetWebsiteData();
+        hasLoaded = websiteData is not null;
+        sortedOtherPages = websiteData?.OtherPages?.OrderBy(x => x.SortOrder);
+        StateHasChanged();
     }
 }
